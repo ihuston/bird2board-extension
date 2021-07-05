deedcda44c0f6fa8568fe928db35c63d69494439
Ian Huston <ian@ianhuston.net>
2021-07-05 21:37:33 +0100

    Trying to get message passing between components.

--- popup.js
+++ popup.js
@@ -2,21 +2,11 @@
 
 let load = document.getElementById("load");
 
-// When the button is clicked, inject function into current page
 load.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: loadTweets,
+    chrome.runtime.sendMessage({
+      tabId: tab.id,
+      message: "button clicked",
     });
 });
-  
-// The body of this function will be executed as a content script inside the
-// current page
-function loadTweets() {
-  // Listen for network events to Twitter Bookmarks API
-
-//    console.log("Started listening.")
-}
-
